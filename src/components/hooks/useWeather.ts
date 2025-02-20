98f015657a8750a51e45719467c5d75ec3290762
ramirolafit5 <ramirolafit5@gmail.com>
2025-02-20 16:34:02 -0300

    error con la url solucionado

--- src/components/hooks/useWeather.ts
+++ src/components/hooks/useWeather.ts
@@ -41,7 +41,7 @@ export default function useWeather() {
         setNotFound(false)
 
         try {
-            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
             
             //metodo get por defecto
             const {data} = await axios(geoUrl)
